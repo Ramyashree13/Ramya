ee96b567c5f8fc5054492d7138bbc043f33c2795
Ramyashree13 <47894457+Ramyashree13@users.noreply.github.com>
2019-06-18 15:51:55 +0530

    Update tuple.py

--- tuple.py
+++ tuple.py
@@ -1,19 +1,39 @@
 def get():
+    
     string=input("enter the string")
-    return str
-
+    return string
+py
 def compute(string):
+    list1=[]
+    n=len(string)
+    for i in range(n):
+        list1.append(string[i])
+    print(list1)
+    r=string.split(';')
+    print(r)
+    a=','.join(r)
+    print(a)
+    b=a.split('=')
+    return b
+    
+    
 
-    list1=string.split(',',1)
-    return list1
+
 
 def output(a):
     print(a)
-
+    
 def main():
     s=get();
     a=compute(s)
     output(a)
 
 main()
+    
+
+    
+        
+    
+
+
     
