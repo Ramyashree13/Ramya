d88f4abe000623372a14350e0aef6e5873f18d81
Ramyashree13 <47894457+Ramyashree13@users.noreply.github.com>
2019-06-18 10:16:44 +0530

    Create tuple.py

---
+++ tuple.py
@@ -0,0 +1,19 @@
+def get():
+    string=input("enter the string")
+    return str
+
+def compute(string):
+
+    list1=string.split(',',1)
+    return list1
+
+def output(a):
+    print(a)
+
+def main():
+    s=get();
+    a=compute(s)
+    output(a)
+
+main()
+    
