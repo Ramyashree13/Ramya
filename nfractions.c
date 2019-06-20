3c99b265018aaad46a7cda8af34531be3ddd8d4f
Ramyashree13 <47894457+Ramyashree13@users.noreply.github.com>
2019-06-20 10:31:25 +0530

    Update nfractions.c

--- nfractions.c
+++ nfractions.c
@@ -1,57 +1,17 @@
 #include<stdio.h>
-
-int gcd(int a[],int *z)
+int gcd(int a,int b)
 {
-        int c,r,i,m,n,res;
-        char true=1;
-        m=a[0];
-        for(i=1;i<*z;i++)
-        {
-                n=a[i];
-                while(true)
-                 {
-                        c=m%n;
-                        if(c==0)
-                        {
-
-                                if(i<*z)
-                                        return n;
-                                else
-                                {
-                                        printf("%d\n",n);
-                                        res=n;
-                                        break;
-                                }
-                        }
-                        /*else if(i==*z)
-                        {
-                                r=c;
-                                printf("gcd=%d\n",r);
-                                return r;
-                                //break;
-                        }*/
-                        else
-                        {
-                                m=n;
-                                n=c;
-                        }
-                }
-                m=res;
-        }
-
-
-
+        if(b!=0)
+                return gcd(b,a%b);
+        else
+                return a;
 }
 int main()
 {
-        int n,a[10],i,b;
-        printf("enter the size\n");
-        scanf("%d",&n);
-        printf("enter the number\n ");
-        for(i=0;i<n;i++)
-                scanf("%d",&a[i]);
-        b=gcd(a,&n);
-        printf("gcd=%d\n",b);
-
+        int a,b,res;
+        printf("enter the two numbers\n");
+        scanf("%d%d",&a,&b);
+        res=gcd(a,b);
+        printf("gcd=%d\n",res);
         return 0;
 }
