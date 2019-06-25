c37114c0c18ef3d5a475e1a5b46b773521b0943e
Ramyashree13 <47894457+Ramyashree13@users.noreply.github.com>
2019-06-25 08:36:41 +0530

    Update nfractions.c

--- nfractions.c
+++ nfractions.c
@@ -21,22 +21,22 @@ int gcd(int a,int b)
         }
 }
 
-void input(struct fraction a[],int *n)
+void input(struct fraction a[])
 {
         int i;
-        printf("enter the size of arraqy\n");
-        scanf("%d",n);
+/*      printf("enter the size of arraqy\n");
+        scanf("%d",n);*/
         printf("enter the fraction number\n");
-        for(i=0;i<*n;i++)
+        for(i=0;i<n;i++)
                 scanf("%d%d",&a[i].num,&a[i].den);
-        for(i=0;i<*n;i++)
+        for(i=0;i<n;i++)
         printf(" %d/%d\n",a[i].num,a[i].den);
 }
-void compute(struct fraction a[],int *n)
+/*void compute(struct fraction a[],int *n)
 {
         /*int i;
         for(i=0;i<*n;i++)
-                printf("%d%d",a[i].num,a[i].den);*/
+                printf("%d%d",a[i].num,a[i].den);
         int gcd1,*num1,*den1,i,p=1,l,b[*n],r=0;
         for(i=0;i<*n;i++)
                 p*= a[i].den;
@@ -54,18 +54,20 @@ void compute(struct fraction a[],int *n)
         printf("gcd=%d\n",gcd1);
         *num1=r/gcd1;
         *den1=p/gcd1;
-
 }
 void output(int *num,int *den)
 {
         printf("the sum of addtion of fractions is=%d/%d\n",*num,*den);
-}
+}*/
 
 
 int main()
 {
-        struct fraction a[5];
+
         int n;
+        printf("enter the size of array\n");
+        scanf("%d",&n);
+        struct fraction a[n];
         input(a,&n);
         compute(a,&n);
         output(&num,&den);
@@ -73,4 +75,4 @@ int main()
 
 }
 
-~
+-- INSERT --                                             
